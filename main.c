183cc2c837eef51d56ccd218a16a84865acfa947
AaronBlancoLopez <72226650+AaronBlancoLopez@users.noreply.github.com>
2023-02-28 18:13:23 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  DCompiler
+//
+//  Created by Aarón Blanco Lopez on 14/2/23.
+//
+
+#include <stdio.h>
+#include "tablaSimbolos.h"
+
+int main(int argc, const char * argv[]) {
+    printf("Hello, World!\n");
+    return 0;
+}
