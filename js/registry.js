67de5884947508601223a0ffa34f220211bb6ba5
AaronBlancoLopez <aaron.blanco@rai.usc.es>
2023-03-27 18:05:27 +0200

    Table colours redefined

--- js/registry.js
+++ js/registry.js
@@ -1,6 +1,7 @@
 var path = '../xml/games.xml';
 var xmlText = null;
 
+//funcion que se encarga leer y devolver el contenido de un fichero xml
 function loadFile(filePath) {
     var result = null;
     var xmlhttp = new XMLHttpRequest();
@@ -12,8 +13,10 @@ function loadFile(filePath) {
     return result;
 }
 
+//carga del xml con el registro de partidas
 xmlContent = loadFile(path)
 
+//funcion de creacion de la tabla de historial de partidas en base al archivo xml
 function loadTable() {
     let games = xmlContent.getElementsByTagName("partida")
     let table = document.getElementsByTagName("tbody")
@@ -21,6 +24,7 @@ function loadTable() {
         const tr = table[0].insertRow()
         for (let j = 0; j < 6; j++) {
             const td = tr.insertCell();
+            //las celdas de jugadores y resultado ocupan dos lineas para corresponder cada jugador con su resultado
             if (j == 1){
                 td.appendChild(document.createTextNode(games[i].children[j].textContent))
                 td.appendChild(document.createElement('br'))
@@ -36,16 +40,18 @@ function loadTable() {
             }
         }
     }
+
+    //llamada a la funcion de estilos de la tabla
     tableStyle()
 }
 
+//funcion que asigna un color a cada fila en funcion del resultado del jugador de la primera fila
 function tableStyle() {
     let rows = document.getElementsByTagName('tr')
     for (let i = 0; i < rows.length - 1; i++) {
         if (rows[i+1].childNodes[2].childNodes[0].textContent == '1') {
             rows[i+1].onmouseover = function() {
                 this.style.backgroundColor = 'green'
-                this.style.filter = 'brightness(80%)'
             }
             rows[i+1].onmouseleave = function() {
                 this.style.backgroundColor = 'black'
@@ -53,7 +59,6 @@ function tableStyle() {
         } else {
             rows[i+1].onmouseover = function() {
                 this.style.backgroundColor = 'red'
-                this.style.filter = 'brightness(80%)'
             }
             rows[i+1].onmouseleave = function() {
                 this.style.backgroundColor = 'black'
